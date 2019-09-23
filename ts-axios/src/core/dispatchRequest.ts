a7e2a3b913cade68a0ff58c636c14c14c0b1270e
Adela2012 <15757124551@163.com>
2019-09-23 11:05:28 +0800

    静态方法扩展

--- ts-axios/src/core/dispatchRequest.ts
+++ ts-axios/src/core/dispatchRequest.ts
@@ -14,13 +14,13 @@ export default function dispatchRequest(config: AxiosRequestConfig): AxiosPromis
 
 // 处理请求参数
 function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformUrl(config)
+  config.url = transformURL(config)
   config.data = transform(config.data, config.headers, config.transformRequest)
   config.headers = flattenHeaders(config.headers, config.method!)
 }
 
 // 实现请求 params 处理逻辑
-function transformUrl(config: AxiosRequestConfig): string {
+export function transformURL(config: AxiosRequestConfig): string {
   let { url, params, paramsSerializer, baseURL } = config
   if (baseURL && !isAbsoluteURL(url!)) {
     url = combineURL(baseURL, url)
