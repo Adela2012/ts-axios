d5b06b6cca3572160692fdb6c0a728015bdd6c50
Adela2012 <15757124551@163.com>
2019-09-26 15:54:51 +0800

    defatults模块单元测试

--- ts-axios/src/defaults.ts
+++ ts-axios/src/defaults.ts
@@ -4,25 +4,32 @@ import { transformRequest, transfromResponse } from './helpers/data'
 
 const defaults: AxiosRequestConfig = {
   method: 'get',
+
   timeout: 0,
+
+  xsrfCookieName: 'XSRF-TOKEN',
+
+  xsrfHeaderName: 'X-XSRF-TOKEN',
+
   headers: {
     common: {
       Accept: 'application/json, text/plain, */*'
     }
   },
-  xsrfCookieName: 'XSRF-TOKEN',
-  xsrfHeaderName: 'X-XSRF-TOKEN',
+
   transformRequest: [
     function (data: any, headers: any): any {
       processHeaders(headers, data)
       return transformRequest(data)
     }
   ],
+
   transformResponse: [
     function (data: any): any {
       return transfromResponse(data)
     }
   ],
+
   validateStatus(status: number): boolean {
     return status >= 200 && status < 300
   }
