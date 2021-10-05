873a49dc47f2656db79010acd42f098e97c962de
Adela2012 <15757124551@163.com>
2021-10-05 23:03:06 +0800

    feat:处理请求 body 数据

--- axios-21/examples/server.js
+++ axios-21/examples/server.js
@@ -9,11 +9,11 @@ const router = express.Router()
 const compiler = webpack(config)
 
 app.use(webpackDevMiddleware(compiler, {
-  publicPath: '/__build__/',
-  stats: {
-    colors: true,
-    chunks: false
-  }
+    publicPath: '/__build__/',
+    stats: {
+        colors: true,
+        chunks: false
+    }
 }))
 
 app.use(webpackHotMiddleware(compiler))
@@ -33,21 +33,38 @@ app.use(router)
 const port = process.env.PORT || 8081
 
 module.exports = app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
+    console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
 })
 
 
 // ------------------------------------ register router
-function registerSimpleRouter () {
-  router.get('/simple/get', function(req, res) {
-    res.json({
-      msg: `hello world`
+function registerSimpleRouter() {
+    router.get('/simple/get', function (req, res) {
+        res.json({
+            msg: `hello world`
+        })
     })
-  })
 }
 
 function registerBaseRouter() {
-    router.get('/base/get', function(req, res) {
+    router.get('/base/get', function (req, res) {
         res.json(req.query)
+    })
+
+    router.post('/base/post', function (req, res) {
+        res.json(req.body)
+    })
+
+    router.post('/base/buffer', function (req, res) {
+        let msg = []
+        req.on('data', (chunk) => {
+            if (chunk) {
+                msg.push(chunk)
+            }
+        })
+        req.on('end', () => {
+            let buf = Buffer.concat(msg)
+            res.json(buf.toJSON())
+        })
     })
 }
--- axios-21/src/index.ts
+++ axios-21/src/index.ts
@@ -1,9 +1,10 @@
 import { AxiosRequestConfig } from "./types";
 import xhr from "./core/xhr";
 import { buildUrl } from "./helpers/url";
+import { transformRequest } from "./helpers/data";
 
 function axios(config: AxiosRequestConfig): void {
-    // processConfig 对 config 中的数据做处理：transformUrl 处理URL
+    // processConfig 对 config 中的数据做处理
     processConfig(config)
 
     // 发送请求
@@ -11,12 +12,19 @@ function axios(config: AxiosRequestConfig): void {
 }
 
 function processConfig(config: AxiosRequestConfig): void {
+    // transformUrl 处理URL
     config.url = transformUrl(config)
+    // transformRequestData 处理 body 逻辑
+    config.data = transformRequestData(config)
 }
 
-function transformUrl(config:AxiosRequestConfig): any{
-    const {url, params} = config
+function transformUrl(config: AxiosRequestConfig): any {
+    const { url, params } = config
     return buildUrl(url, params)
+}
+
+function transformRequestData(config:AxiosRequestConfig): any{
+    return transformRequest(config.data)
 }
 
 export default axios
